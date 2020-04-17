7a275ed3ac46e79d88bf931e823571144e55a33d
sruthy krishnan <51168145+sruthykrishnan@users.noreply.github.com>
2020-04-18 04:22:05 +0530

    Quick Sort Algorithm

---
+++ quick_sort.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+int compar(const void *pa, const void *pb)
+{
+int a = *(const int *)pa;
+int b = *(const int *)pb;
+int ret = 0;
+if(a<b)
+ret = -1;
+else if(a>b)
+ret = 1;
+return ret;
+}
+int main()
+{
+int myarr[] = {8, 32, -76, 4025, 0, 3, -329, 5412, 6};
+size_t L = sizeof(myarr)/sizeof(int);
+printf(" Numbers before sorting\n");
+int i;
+for(i=0; i<L; ++i)
+printf(" %d", myarr[i]);
+printf("\n");
+
+qsort((void *)myarr, L, sizeof(int), compar);
+printf(" Numbers after sorting\n");
+for(i=0; i<L; ++i)
+printf(" %d", myarr[i]);
+return 0;
+}
